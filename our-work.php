091c2fe6ccf3c50ab150446d4f82a7c8ea848de5
Caleb Nance <calebnance@gmail.com>
2016-11-03 09:26:05 -0400

    more work on the our work page

--- our-work.php
+++ our-work.php
@@ -16,22 +16,32 @@ include('includes/menu.php');
 <h2>Signage</h2>
 <div class="row">
   <div class="col-xs-12 col-sm-3">
-    <img src="https://placekitten.com/140" alt="alt text" class="img-thumbnail img-responsive">
+    <img src="https://placekitten.com/240" alt="alt text" class="img-thumbnail img-responsive">
   </div><!-- /.col-xs-12 -->
   <div class="col-xs-12 col-sm-3">
-    <img src="https://placekitten.com/140" alt="alt text" class="img-thumbnail img-responsive">
+    <img src="https://placekitten.com/240" alt="alt text" class="img-thumbnail img-responsive">
   </div><!-- /.col-xs-12 -->
   <div class="col-xs-12 col-sm-3">
-    <img src="https://placekitten.com/140" alt="alt text" class="img-thumbnail img-responsive">
+    <img src="https://placekitten.com/240" alt="alt text" class="img-thumbnail img-responsive">
   </div><!-- /.col-xs-12 -->
   <div class="col-xs-12 col-sm-3">
-    <img src="https://placekitten.com/140" alt="alt text" class="img-thumbnail img-responsive">
+    <img src="https://placekitten.com/240" alt="alt text" class="img-thumbnail img-responsive">
   </div><!-- /.col-xs-12 -->
 </div><!-- /.row -->
 
 <h2>Other Examples</h2>
 
 <h2>Our Tools</h2>
+<div class="row">
+  <div class="col-xs-12 col-sm-3">
+    <div class="thumbnail">
+      <img src="/img/truvdesign_thumb.jpg" alt="Our Tools - Shopbot" class="img-responsive">
+      <div class="caption">
+        <h3 class="mt-0">Shopbot</h3>
+      </div><!-- /.caption -->
+  </div><!-- /.col-xs-12 -->
+</div><!-- /.row -->
+
 
 <?php
 include('includes/foot.php');

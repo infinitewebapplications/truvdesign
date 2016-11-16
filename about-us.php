8ee820cce4458ceeb7fda4b0b334b224926d5f8c
Caleb Nance <calebnance@gmail.com>
2016-11-16 18:10:56 -0500

    updated the copy

--- about-us.php
+++ about-us.php
@@ -13,43 +13,48 @@ include('includes/menu.php');
 
 <h1>About Us</h1>
 
-<img src="/img/david-and-paul-nance-truvdesign_thumb.jpg" alt="Paul and David Nance" title="Paul and David Nance" class="img-responsive img-thumbnail about-us-img">
+<div class="row">
+  <div class="col-xs-12 col-sm-4">
+    <a rel="g1" class="swipebox" href="/img/david-and-paul-nance-truvdesign.jpg" title="David and Paul Nance">
+      <img src="/img/david-and-paul-nance-truvdesign_thumb.jpg" alt="David and Paul Nance" title="David and Paul Nance" class="img-responsive img-thumbnail">
+    </a>
+  </div><!-- /.col-xs-12 -->
+  <div class="col-xs-12 col-sm-8">
+    <p>After working individually for 30+ years in the industry, brothers David and
+    Paul Nance, decided to combine their skills and talent to start TruVDesign.
+    TruVDesign specializes in various wood items, customized to your needs. To
+    create one-of-a-kind pieces, they use a CNC woodworking maching ("Shopbot")
+    which allows them to customize everything from simple signs to more complex
+    carvings. If you need a sign for your commercial business, wedding, any
+    furniture pieces or home d&eacute;cor – TruVDesign is able to accommodate your needs
+    and provide highquality work with every request. Learn more about David and
+    Paul below.</p>
+  </div><!-- /.col-xs-12 -->
+</div>
 
 <h2>David Nance</h2>
-<p>Art is not a separate thing in my life, everything I see is through
-eyes that work on painting possibilities. How would I go about capturing
-that? How could I see that differently?</p>
-<p>The fact that I can be moved by art has always fascinated me, the
-way I can be drawn into more than what I am seeing. It is like when a
-piece of music takes me to a time or place.</p>
-<p>When I am painting I am inspired by the quality of light, how it defines
-and sculpts the subject. I have been using oil paints for the past few
-years to give me control over hard and soft edges. There is a little more
-blending time with the oils. There is also a luminous quality that
-responds well to chasing the light.</p>
-<p>Over time I have developed a vocabulary of brushwork to describe what
-I see and express feeling in my work. I like using familiar subjects
-and paint them in a way that leaves something for the imagination.</p>
+<p>David has always had a strong passion for wood work, painting and all the
+creativity it allows for. Being able to evoke emotions thorugh his work is
+something he takes great pride in. Having worked in the industry for over 30+
+years, David is extremely talented in conceptualizing and creating unique pieces
+of art.</p>
 
 <h2>Paul Nance</h2>
-<p>My interests have always been in mechanical design and curious of
-how things worked. I started my design in textile industry, drafting
-and designing looms, narrow fabric knitting machines. After the textile
-industry changed I moved into more wood and metal working machine
-design, as technology changed the need to move from drafting board type
-work to computer generated design using AutoCAD and SolidWorks
-type software.</p>
-<p>After working for more than 30 years in the industry, it was decided
-to collaborate with my brother (David) and start a business combining
-our creative experiences to design and make various wood items.</p>
-<p>We chose a CNC woodworking machine "Shopbot" to allow us to create
-simple and complex designs. Starting out with signage for commercial use,
-adding personalized items for weddings, home decor and simple furniture
-pieces.</p>
-<p>Look over the <a href="/our-work">Our Work</a> page for items that we have
-completed or are currently working on. We can help with small
-production runs if there are examples you can provide we will be glad to quote.</p>
-<p>Let us know if there is anything we can help you with!</p>
+<p>Paul has always had a strong interest for mechanical design which led him to
+start his career in the textile industry over 30+ years ago. There, he drafted
+and designed looms, narrow fabric machines and other complex machinery.
+Following his carrer in the textile industry, due to significant industrial
+changes, he shifted his focus to wood and metal machinery design. As technology
+advanced and evolved, Paul adapted and moved the drafting board work to
+computer generated designs using AutoCAD and SolidWorks software.</p>
+
+<br><br><br>
+
+<p>See the <a href="/our-work">Our Work</a> page for sample items we've created
+and are currently working on completing. We are happy to accommodate small
+production runs or complex projects on a quote by quote basis. Please
+<a href="/contact-us">contact us</a> for your personalized quote today
+<i>(please provide details of your needs when submitting a quote request)</i>.</p>
 
 <?php
 include('includes/foot.php');
